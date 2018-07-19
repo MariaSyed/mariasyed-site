1d89d85dd4d1e95cab617e9f21c8a1ca5feb3b4f
MariaSyed <mariasy@metropolia.fi>
2018-07-19 23:23:49 +0300

    Added transitions and animations to card

--- src/js/components/Intro.js
+++ src/js/components/Intro.js
@@ -12,30 +12,43 @@ import EmailIcon from 'grommet/components/icons/base/SocialMail'
 // import image from '../../../public/img/maria.png'
 
 const config = {
-  moveUp: {
-    top: 5
-  },
-  moveDown: {
-    top: 15
-  }
+  visible: { opacity: 1, transition: { duration: 1100 } },
+  hidden: { opacity: 0, transition: { duration: 1100 } }
 }
 
 const AnimatedBox = posed.div(config)
 
 export default class Intro extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      card1Visible: false,
+      card2Visible: false,
+      card3Visible: false
+    }
+  }
+
+  componentWillMount() {
+    setTimeout(() => this.setState({ card1Visible: true }), 800)
+    setTimeout(() => this.setState({ card2Visible: true }), 1200)
+    setTimeout(() => this.setState({ card3Visible: true }), 1800)
+
+  }
+
   render() {
     return (
       <AppSection colorIndex="light-1">
         <div className="main-content">
+          <h1 className="title">This is Maria.</h1>
           <div className="cards-container">
-            <AnimatedBox className="card">
+            <AnimatedBox className="card" pose={this.state.card1Visible ? 'visible' : 'hidden'}>
               <p className="card-title">Adventurer</p>
             </AnimatedBox>
-            <AnimatedBox className="card">
+            <AnimatedBox className="card" pose={this.state.card2Visible ? 'visible' : 'hidden'}>
               <p className="card-title">Developer</p>
             </AnimatedBox>
-            <AnimatedBox className="card">
-              <p className="card-title">Explorer</p>
+            <AnimatedBox className="card" pose={this.state.card3Visible ? 'visible' : 'hidden'}>
+              <p className="card-title">Belcher</p>
             </AnimatedBox>
           </div>
 
