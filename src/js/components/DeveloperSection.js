69e5c49650e8727b319a71d245f384992af8e3f5
MariaSyed <mariasy@metropolia.fi>
2018-10-07 21:58:39 +0300

    Prettier format

--- src/js/components/DeveloperSection.js
+++ src/js/components/DeveloperSection.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react'
-import AppSection from "./AppSection";
+import React, { Component } from 'react'
+import AppSection from './AppSection'
 import { Heading } from 'grommet'
 
 export class DeveloperSection extends Component {
   render() {
     return (
-      <AppSection id="developer-section" colorIndex="light-2" full='vertical'>
+      <AppSection id="developer-section" colorIndex="light-2" full="vertical">
         <Heading>Developer things here</Heading>
       </AppSection>
     )
--- src/js/components/Section3.js
+++ src/js/components/Section3.js
@@ -7,16 +7,23 @@ import GitHubIcon from 'grommet/components/icons/base/SocialGithub'
 export default class Section3 extends Component {
   render() {
     return (
-      <AppSection id="section-3" colorIndex="grey-1-a" basis="large" a11yTitle="Section 2">
+      <AppSection
+        id="section-3"
+        colorIndex="grey-1-a"
+        basis="large"
+        a11yTitle="Section 2"
+      >
         <Box>
           <Heading size="small">Check out this site on GitHub</Heading>
-          <Button icon={<GitHubIcon />}
-                  label='Check'
-                  onClick={() => {}}
-                  href='#'
-                  primary={true}
-                  critical={false}
-                  accent={false} />
+          <Button
+            icon={<GitHubIcon />}
+            label="Check"
+            onClick={() => {}}
+            href="#"
+            primary={true}
+            critical={false}
+            accent={false}
+          />
         </Box>
       </AppSection>
     )
